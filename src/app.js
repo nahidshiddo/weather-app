32efebfe30a56e8488321264a68f877970795363
Nahid Shiddo <100500227+nahidshiddo@users.noreply.github.com>
2022-03-10 22:20:28 -0500

    Added feature to the forecast section

--- src/app.js
+++ src/app.js
@@ -22,6 +22,43 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+        <div class="forecast-date">${day}</div>
+        <img
+          src="https://openweathermap.org/img/wn/50n@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="forecast-temperature">
+          <span class="forecast-max">18°</span>
+          <span class="forecast-min">12°</span>
+        </div>
+      </div>
+    
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "2c8ecbb7e9c5667198477d206da6c0f1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -45,6 +82,8 @@ function displayTemperature(response) {
   );
 
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
